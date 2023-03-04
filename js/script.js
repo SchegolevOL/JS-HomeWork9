7633dbd90a4f40ed92787854a5fcfcc28d1d7b58
Oleg Schegol <olschegolev@yahoo.com>
2023-03-04 23:01:17 +0500

    mod

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
-let arr1 = [-1, 4, 7, 7, 9, 10, 10, 20, 30, 30, 30, 41,44];
-let arr2 = [0, 3, 4, 5, 6, 7, 11, 20, 20, 50,60,100,100,120,120,150];
-let arr3 = FuncSummArr(arr1, arr2);
+let arr1 = [-1,0,1,11,15,16,17];
+let arr2 = [0,3,4,5,6,7,8,9,10,11,12,13,14];
+let arr3 = FuncSummArr1(arr1, arr2);
+
 
 console.log(`Array:  ${arr3}`);
 
-// function FuncSummArr(arr1, arr2) {
-//   let arr = [];
-//   let lengthArr1 = arr1.length;
-//   let lengthArr2 = arr2.length;
-//   let i = 0;
-//   let j = 0;
-//   while (lengthArr1 + lengthArr2 != 0) {
-//     if (arr1[i] < arr2[j] && lengthArr1 != 0) {
-//       arr.push(arr1[i]);
-//       i++;
-//       lengthArr1--;
-//     } else if (lengthArr2 != 0) {
-//       arr.push(arr2[j]);
-//       j++;
-//       lengthArr2--;
-//     }
-//   }
-//   return arr;
-// }
 
-function FuncSummArr(arr1, arr2) {
+function FuncSummArr1(arr1, arr2) {
   let res = [];
-  let i = -1;
-  let j = -1;
+  let i = 0;
+  let j = 0;
   // В одну строчку
-  while ((arr1.length + arr2.length)!= (i + j)+1) res.push(arr1[i+1] < arr2[j+1] && i!=arr1.length ? arr1[++i] : arr2[++j]) ;  
+  while (arr1.length + arr2.length != i + j) {
+    arr1[i] < arr2[j] && i != arr1.length
+      ? res.push(arr1[i++])
+      : j != arr2.length
+      ? res.push(arr2[j++])
+      : res.push(arr1[i++]);
+  }
+
   return res;
 }
-
